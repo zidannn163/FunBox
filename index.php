0fff5ff2e0acb2786fbc59465c013527acfe432c
zidannn163 <zidannn163@gmail.com>
2018-10-24 01:32:30 +0300

    final

--- index.php
+++ index.php
@@ -5,82 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>FunBox</title>
 </head>
 <body>
     <div class="back">
         <div class="center">
             <h1>Ты сегодня покормил кота?</h1>
             <div class="nyams">
-                <div class="nyam">
-                    <div class="nyam-border default">
-                        <div class="nyam-block">
-                            <div class="nyam-block-title">
-                                <p>Сказачное заморское яство</p>
-                                <h2>Нямушка</h2>
-                                <p>с рыбой</p>
-                            </div>
-                            <div class="nyam-block-container">
-                                <p><span>40</span> порций</p>
-                                <p><span>2</span> мыши в подарок</p>
-            
-                            </div>
-                            <img src="img/funbox_logo.png" alt="">
-                            <div class="nyam-block-circle">
-                                
-                                <p><span>2</span><br> кг</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="nyam-block-text"> да свежайшая сёмушка</div>
-                </div>
-                <div class="nyam">
-                        <div class="nyam-border default">
-                            <div class="nyam-block">
-                                <div class="nyam-block-title">
-                                    <p>Сказачное заморское яство</p>
-                                    <h2>Нямушка</h2>
-                                    <p>с рыбой</p>
-                                </div>
-                                <div class="nyam-block-container">
-                                    <p><span>40</span> порций</p>
-                                    <p><span>2</span> мыши в подарок</p>
-                
-                                </div>
-                                <img src="img/funbox_logo.png" alt="">
-                                <div class="nyam-block-circle">
-                                    
-                                    <p><span>2</span><br> кг</p>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="nyam-block-text">Головы щучьи с чесноком да свежайшая сёмушка</div>
-                    </div>
-                <div class="nyam">
-                            <div class="nyam-border default">
-
-                                <div class="nyam-block">
-                                    <div class="nyam-block-title">
-                                        <p>Сказачное заморское яство</p>
-                                        <h2>Нямушка</h2>
-                                        <p>с рыбой</p>
-                                    </div>
-                                    <div class="nyam-block-container">
-                                        <p><span>40</span> порций</p>
-                                        <p><span>2</span> мыши в подарок</p>
-                                        <p>Заказчик доволен</p>
-                                    </div>
-                                    <img src="img/funbox_logo.png" alt="">
-                                    <div class="nyam-block-circle">
-                                        
-                                        <p><span>2</span><br> кг</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="nyam-block-text">Головы щучьи с чесноком да свежайшая сёмушка</div>
-                    </div>
+               
     
             </div>
 
@@ -88,6 +20,7 @@
     </div>
 
     <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/renderNyam.js"></script>
     <script src="js/main.js"></script>
 </body>
 </html>
